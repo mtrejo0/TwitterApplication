bd4b45212b056ad38f0e4cb53a41bae651011e81
Moises Trejo <moisest@mit.edu>
2019-12-26 20:50:32 -0600

    new gif

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeFragment.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeFragment.java
@@ -54,7 +54,7 @@ public class ComposeFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_compose, container);
+        return inflater.inflate(R.layout.fragment_detail, container);
     }
 
     @Override
